dbd5ccdb622e52c1f00f3b087b5832b7f92b6b5e
Bhaskar11111 <bhaskarmishra911@gmail.com>
2026-07-18 00:44:07 +0530

    actual URL 1.2

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cookieParser=require('cookie-parser')
 const morgan=require('morgan')
 
 const corsOptions={
-    origin:'https://hello-etos.onrender.com/',
+    origin:'https://hello-etos.onrender.com',
     methods:['GET','POST', 'PUT', 'DELETE'],
     optionSuccessStatus:200,
     credentials:true
